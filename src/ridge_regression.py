575e21559e6f0a95caf9de468e7f09f5fdc5437d
James Kim <biojameskim2002@gmail.com>
2024-03-07 22:59:10 -0500

    Create [create_predicted_responses_dict] function
    - Created separate function for creating a dictionary of predicted responses because there was no need to calculate the average for the predicted activations using [create_averaged_responses_dict] since given the same image, the 3 predicted responses are all the same.
    - This is different from the measured responses though because given the same image, those activations are different which is why we still need to calculate the average for the measured responses.

--- src/ridge_regression.py
+++ src/ridge_regression.py
@@ -49,8 +49,10 @@ def create_averaged_responses_dict(ordering_array, responses):
     [create_averaged_responses_dict] creates a dictionary where the key is the 
     order of the image and the value is the average response of the image.
 
-    For example, order_to_responses = {..., 625: [response1, response2, response3], ...}
-    would become: order_to_responses = {..., 625: [np.mean(response1 + response2 + response3)], ...}
+    For example, the first loop create the dict:    
+        [order_to_responses = {..., 625: [response_1, response_2, response_3], ...}]
+    The second loop averages the responses to get the average measured response for each order:
+        [order_to_responses = {..., 625: [np.mean(response_1 + response_2 + response_3)], ...}]
     where each response is a 241-dim vector.
     """
     order_to_responses = {}
@@ -59,7 +61,8 @@ def create_averaged_responses_dict(ordering_array, responses):
     # create a dictionary where key is the order and value is the list of responses
     # For example, order_to_responses = {625: [response1, response2, response3], ...}
     # where each response is a 241-dim vector.
-    for i in range(len(responses)):
+
+    for i in range(len(ordering_array)):
         key = ordering_array[i]
         if key in order_to_responses:
             order_to_responses[key].append(responses[i])
@@ -70,6 +73,24 @@ def create_averaged_responses_dict(ordering_array, responses):
     # For example, order_to_responses = {625: [(response1 + response2 + response3) / 3], ...}
     for key in order_to_responses:
         order_to_responses[key] = np.mean(order_to_responses[key], axis=0)
+    return order_to_responses
+
+def create_predicted_responses_dict(ordering_array, responses):
+    """
+    [create_predicted_responses_dict] creates a dictionary where the key is the
+    order of the image and the value is the predicted response of the image.
+
+    Since the predicted response is the same for each image, we can just take the
+    first response and use that as the predicted response.
+    For example, order_to_responses = {..., 624: [response_1], 625: [response_1], ...}
+    """
+    order_to_responses = {}
+
+    for i in range(len(ordering_array)):
+        key = ordering_array[i]
+        if key not in order_to_responses:
+            order_to_responses[key] = responses[i]
+    
     return order_to_responses
 
 def plot_validation_response(model, matched_clip_vectors, activation, train_mask):
@@ -86,7 +107,7 @@ def plot_validation_response(model, matched_clip_vectors, activation, train_mask
     # validation set has 3000 trials
     ordering_array = ordering[~train_mask]
 
-    avg_predicted_response_dict = create_averaged_responses_dict(ordering_array, predicted_response)
+    avg_predicted_response_dict = create_predicted_responses_dict(ordering_array, predicted_response)
     avg_measured_response_dict = create_averaged_responses_dict(ordering_array, measured_response)
     
     # next step is to convert these dictionaries into arrays and plot them
@@ -134,6 +155,6 @@ if __name__ == "__main__":
         scoring='r2'
     )
 
-    # print_model_metrics(model)
+    # print_model_metrics(model, matched_clip_vectors, activation, train_mask)
   
     plot_validation_response(model, matched_clip_vectors, activation, train_mask)

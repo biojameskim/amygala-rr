7e755f93a1ae19ec3c757d3b00822b55306348f4
James Kim <biojameskim2002@gmail.com>
2024-03-15 11:58:41 -0400

    Refactor code for creating dictionaries
    - Separate averaging values of dictionary from creating the dictionary
    - Add option for sorting dictionaries by keys

--- src/plot_predicted_measured_responses.py
+++ src/plot_predicted_measured_responses.py
@@ -1,23 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def create_averaged_responses_dict(ordering_array, responses):
+def create_responses_dict(ordering_array, responses, sort=False):
     """
-    [create_averaged_responses_dict] creates a dictionary where the key is the 
-    order of the image and the value is the average response of the image.
+    [create_responses_dict] creates a dictionary where the key is the 
+    order of the image and the values are the three responses of the image.
+    If [sort] is True, then the dictionary is sorted by key.
 
-    For example, the first loop create the dict:    
-        [order_to_responses = {..., 625: [response_1, response_2, response_3], ...}]
-    The second loop averages the responses to get the average measured response for each order:
-        [order_to_responses = {..., 625: [np.mean(response_1 + response_2 + response_3)], ...}]
-    where each response is a 241-dim vector.
+    For example, [order_to_responses = {..., 625: [response_1, response_2, response_3], ...}]
+    where each response is a 241-dim vector. 
     """
     order_to_responses = {}
-
-    # each order should have three responses
-    # create a dictionary where key is the order and value is the list of responses
-    # For example, order_to_responses = {625: [response1, response2, response3], ...}
-    # where each response is a 241-dim vector.
 
     for i in range(len(ordering_array)):
         key = ordering_array[i]
@@ -26,16 +19,26 @@ def create_averaged_responses_dict(ordering_array, responses):
         else:
             order_to_responses[key] = [responses[i]]
 
-    # iterate through the dictionary and average the responses to get the average measured response for each order
-    # For example, order_to_responses = {625: [(response1 + response2 + response3) / 3], ...}
-    for key in order_to_responses:
-        order_to_responses[key] = np.mean(order_to_responses[key], axis=0)
+    if sort:
+        return dict(sorted(order_to_responses.items()))
     return order_to_responses
 
-def create_predicted_responses_dict(ordering_array, responses):
+def average_dictionary_values(dictionary):
+    """
+    [average_dictionary_values] averages the values of the dictionary.
+
+    For example, [order_to_responses = {..., 625: [np.mean(response_1 + response_2 + response_3)], ...}]
+    where each response is a 241-dim vector.
+    """
+    for key in dictionary:
+        dictionary[key] = np.mean(dictionary[key], axis=0)
+    return dictionary
+
+def create_predicted_responses_dict(ordering_array, responses, sort=False):
     """
     [create_predicted_responses_dict] creates a dictionary where the key is the
     order of the image and the value is the predicted response of the image.
+    If [sort] is True, then the dictionary is sorted by key.
 
     Since the predicted response is the same for each image, we can just take the
     first response and use that as the predicted response.
@@ -47,6 +50,9 @@ def create_predicted_responses_dict(ordering_array, responses):
         key = ordering_array[i]
         if key not in order_to_responses:
             order_to_responses[key] = responses[i]
+
+    if sort:
+        return dict(sorted(order_to_responses.items()))
     
     return order_to_responses
 
@@ -56,16 +62,17 @@ def plot_validation_response(model, matched_clip_vectors, activation, train_mask
     average measured response for each image in the validation set.
     """
 
-    predicted_response = model.predict(matched_clip_vectors[~train_mask])
-    measured_response = activation[~train_mask]
-    # shape of predicted response (3000, 241)
-    # shape of measured response (3000, 241)
+    # ~train_mask is the validation set
+    predicted_responses = model.predict(matched_clip_vectors[~train_mask])
+    measured_responses = activation[~train_mask]
+    # shape of predicted responses (3000, 241)
+    # shape of measured responses (3000, 241)
 
     # validation set has 3000 trials
     ordering_array = ordering_array[~train_mask]
 
-    predicted_response_dict = create_predicted_responses_dict(ordering_array, predicted_response)
-    avg_measured_response_dict = create_averaged_responses_dict(ordering_array, measured_response)
+    predicted_response_dict = create_predicted_responses_dict(ordering_array, predicted_responses)
+    avg_measured_response_dict = average_dictionary_values(create_responses_dict(ordering_array, measured_responses))
     
     # next step is to convert these dictionaries into arrays and plot them
     predicted_response_array = np.array(list(predicted_response_dict.values()))

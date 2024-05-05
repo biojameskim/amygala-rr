e4bc15eae1d920f9902527a9bd5e01803d25c5fc
James Kim <biojameskim2002@gmail.com>
2024-05-05 17:56:59 -0400

    Update noise_ceiling.py
    - No longer averages 2 measured responses and correlates with the 3rd because not all subjects see all images 3 repetitions

--- src/noise_ceiling.py
+++ src/noise_ceiling.py
@@ -5,14 +5,20 @@ import ridge_regression as rr
 import matplotlib.pyplot as plt
 from scipy import stats
 
-def create_noise_ceiling(model, matched_clip_vectors, train_mask, validation, val_ordering_array):
+def create_noise_ceiling_avg(model, matched_clip_vectors, num_voxels, train_mask, validation, val_ordering_array):
     """
-    1. For each image in the validation set, randomly choose 2 of the 3 measured responses and average them.
-    2. For the remaining response, calculate the correlation between the average measured response and the remaining response.
-    3. Get the predicted response for each image in the validation set. Calculate the correlation
-    between the average measured response and the predicted response.
+    [create_noise_ceiling] returns 3 arrays: averaged_responses, remaining_responses, predicted_responses.
+    This function does NOT actually calculate the correlations but outlines the process for doing so here:
 
-    First, create a dictionary where the key is the order of the image and the values are the 3 responses for each image.
+    1. For each image in the validation set, randomly choose 2 of the measured responses and average them.
+        - We store these averages in [averaged_responses]
+    2. For the remaining response, calculate the correlation between the average measured response and the remaining response.
+        - We store these remaining responses in [remaining_responses]
+    3. Get the predicted response for each image in the validation set. Calculate the correlation between the average measured response and the predicted response.
+        - We store these predicted responses in [predicted_responses]
+
+    First, create a dictionary where the key is the order of the image and the values are the responses for each image.
+    Images have 1-3 responses based on how many times they were shown to each subject.
     """
     all_predicted_responses = model.predict(matched_clip_vectors[~train_mask])
     # create a dictionary where the key is the order of the image and the value is the predicted response of the image
@@ -23,17 +29,19 @@ def create_noise_ceiling(model, matched_clip_vectors, train_mask, validation, va
     # create a dictionary where the key is the order of the image and the values are the 3 responses for each image
     # Sort the dict by key to preserve the order of the responses so we can perform correlation later
     response_dict = ppr.create_responses_dict(val_ordering_array, validation, sort=True) 
-    
-    # iterate through the dictionary and randomly choose 2 of the 3 measured responses, average them, and put them in an array. Put the remaining one in a separate array.
-    # For example, order_to_responses = {625: [average_response, remaining_response], ...}
 
     # The order should be preserved as the indices are the same for averaged_responses and remaining_responses
-    averaged_responses = np.empty((1000, 241))
-    remaining_responses = np.empty((1000, 241))
+    averaged_responses = np.empty((1000, num_voxels))
+    remaining_responses = np.empty((1000, num_voxels))
 
     for i, key in enumerate(response_dict):    
-        values = response_dict[key] 
-        random_indices = np.random.choice(3, 2, replace=False) # 2 random indices
+        values = response_dict[key]
+        num_values = len(values)
+        if num_values >= 2: # If there are at least 2 values, choose 2 without replacement
+            random_indices = np.random.choice(num_values, 2, replace=False) # 2 random indices
+        else: # If there is only 1 value, choose it twice
+            random_indices = [0, 0]
+
         random_choices = [values[i] for i in random_indices] # extract the 2 random choices
         average_response = np.mean(random_choices, axis=0) # average the 2 random choices
         remaining_response = np.delete(values, random_indices, axis=0)  # remove the 2 random choices from the array and get the remaining response
@@ -42,7 +50,50 @@ def create_noise_ceiling(model, matched_clip_vectors, train_mask, validation, va
 
     return averaged_responses, remaining_responses, predicted_responses
 
-def calculate_corr_and_pvalue(resp1, resp2):
+
+def create_noise_ceiling(model, matched_clip_vectors, num_voxels, train_mask, validation, val_ordering_array):
+    """
+    [create_noise_ceiling] returns 3 arrays: responses_1, responses_2, predicted_responses.
+    This function does NOT actually calculate the correlations but outlines the process for doing so here.
+    Once we get these arrays, we can calculate the correlation between the measured responses (responses_1) and the predicted responses.
+
+    1. For each image in the validation set, randomly choose 2 of the measured responses.
+        - We store one in [responses_1] and the other in [responses_2]
+    2. Get the predicted response for each image in the validation set.
+        - We store these predicted responses in [predicted_responses]
+
+    First, create a dictionary where the key is the order of the image and the values are the responses for each image.
+    Images have 1-3 responses based on how many times they were shown to each subject.
+    """
+    all_predicted_responses = model.predict(matched_clip_vectors[~train_mask])
+    # create a dictionary where the key is the order of the image and the value is the predicted response of the image
+    predicted_responses_dict = ppr.create_predicted_responses_dict(val_ordering_array, all_predicted_responses, sort=True)
+    # convert the sorted dictionary values to an array
+    predicted_responses = np.array(list(predicted_responses_dict.values()))
+
+    # create a dictionary where the key is the order of the image and the values are the responses for each image
+    # Sort the dict by key to preserve the order of the responses so we can perform correlation later
+    response_dict = ppr.create_responses_dict(val_ordering_array, validation, sort=True) 
+
+    # The order should be preserved as the indices are the same for averaged_responses and remaining_responses
+    responses_1 = np.empty((1000, num_voxels))
+    responses_2 = np.empty((1000, num_voxels))
+
+    for i, key in enumerate(response_dict):    
+        values = response_dict[key] 
+        num_values = len(values)
+        if num_values >= 2: # If there are at least 2 values, choose 2 without replacement
+            random_indices = np.random.choice(num_values, 2, replace=False) # 2 random indices
+        else: # If there is only 1 value, choose it twice
+            random_indices = [0, 0]
+
+        random_choices = [values[i] for i in random_indices] # extract the 2 random choices
+        responses_1[i] = random_choices[0]
+        responses_2[i] = random_choices[1]
+
+    return responses_1, responses_2, predicted_responses
+
+def calculate_corr_and_pvalue(resp1, resp2, num_voxels):
     """
     [calculate_corr_and_pvalue] calculates the correlations and pvalues between responses 1 and 2.
     
@@ -52,17 +103,17 @@ def calculate_corr_and_pvalue(resp1, resp2):
     Because we want to see how accurate the prediction at each voxel is, each entry
     in the return vector is the correlation at each voxel.
 
-    averaged_responses.shape = (1000, 241)
-    remaining_responses.shape = (1000, 241)
-    predicted_responses.shape = (1000, 241)
+    averaged_responses.shape = (1000, num_voxels)
+    remaining_responses.shape = (1000, num_voxels)
+    predicted_responses.shape = (1000, num_voxels)
 
-    Returns [correlations] correlation vector of size (241, 1)
-    Returns [p_values] p-value vector of size (241, 1)
+    Returns [correlations] correlation vector of size (num_voxels, 1)
+    Returns [p_values] p-value vector of size (num_voxels, 1)
     """
-    correlations = np.empty((241, 1))
-    p_values = np.empty((241, 1))
+    correlations = np.empty((num_voxels, 1))
+    p_values = np.empty((num_voxels, 1))
 
-    for i in range(0,241):
+    for i in range(0, num_voxels):
         corr, p = stats.pearsonr(resp1[:, i], resp2[:, i])
         correlations[i] = corr
         p_values[i] = p
@@ -92,33 +143,27 @@ def plot_correlations(correlations, correlations2):
     plt.ylabel("Correlation")
     plt.title("Correlation Densities")
     
-    plt.xticks([x_pos1, x_pos2], ['average vs remaining', 'average vs predicted'])
+    plt.xticks([x_pos1, x_pos2], ['measured_1 vs measured_2', 'measured_1 vs predicted'])
 
     plt.show()
 
 
 if __name__ == "__main__":
 
-    matched_clip_vectors = rr.match_ordering(rr.ordering, rr.clip_vectors)
+    subject_number, roi_data_file, clip_vector_file = rr.get_files_from_arguments()
 
-    model = rr.train_ridge_regression_cv_model(
-        matched_clip_vectors[rr.train_mask],
-        rr.activation[rr.train_mask], 
-        alphas=[1e-3], 
-        n_splits=5, 
-        n_repeats=3,
-        random_state=None,
-        scoring='r2'
-    )
+    train_mask, ordering, model, activation, matched_clip_vectors = rr.ridge_regression(int(subject_number), roi_data_file, clip_vector_file)
 
-    validation = rr.activation[~rr.train_mask]
-    val_ordering_array = rr.ordering[~rr.train_mask]
+    validation = activation[~train_mask]
+    val_ordering_array = ordering[~train_mask]
 
-    averaged_responses, remaining_responses, predicted_responses = create_noise_ceiling(
-        model, matched_clip_vectors, rr.train_mask, validation, val_ordering_array)
+    num_voxels = activation.shape[1] # number of voxels in this ROI
+
+    responses_1, responses_2, predicted_responses = create_noise_ceiling(
+        model, matched_clip_vectors, num_voxels, train_mask, validation, val_ordering_array)
     
-    observed_correlations, pvals1 = calculate_corr_and_pvalue(averaged_responses, remaining_responses)
-    predicted_correlations, pvals2 = calculate_corr_and_pvalue(averaged_responses, predicted_responses)
+    observed_correlations, pvals1 = calculate_corr_and_pvalue(responses_1, responses_2, num_voxels)
+    predicted_correlations, pvals2 = calculate_corr_and_pvalue(responses_1, predicted_responses, num_voxels)
 
     plot_correlations(observed_correlations, predicted_correlations)
 
